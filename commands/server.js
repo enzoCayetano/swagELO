999224e19e134ab679f97e0af5959f37d1727a97
Enzo Cayetano <cayetano.enzo@icloud.com>
2023-10-19 21:58:35 -0700

    Added eslint and commands

---
+++ commands/server.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('About this server!'),
+    async execute(interaction) {
+        await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`)
+    },
+};

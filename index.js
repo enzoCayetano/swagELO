c0d740fbe43f3678e6e10b9562cd9ea065362069
Enzo Cayetano <cayetano.enzo@icloud.com>
2023-10-19 17:40:46 -0700

    Added classes and initial code to get started

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// Require necessary discord.js classes and .env
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv')
-
 dotenv.config()
 
-console.log(process.env.DISCORD_TOKEN)
+// Create new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(process.env.DISCORD_TOKEN)

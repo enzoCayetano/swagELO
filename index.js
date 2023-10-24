6e228448f6390126aa61a45232d04f0da079c2d7
Enzo Cayetano <cayetano.enzo@icloud.com>
2023-10-24 10:27:33 -0700

    Added way to get sorted commands in subfolders

--- index.js
+++ index.js
@@ -15,20 +15,27 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 // Create commands
 client.commands = new Collection()
 
-// Get commands '.js' from commands folder
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+// Get subfolders path
+const foldersPath = path.join(__dirname, 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
 
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath)
-
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ('data' in command && 'execute' in command) {
-        client.commands.set(command.data.name, command)
-    } else {
-        console.log(`Warning! The command at ${filePath} is missing a required "data" or "execute" property!`)
-    }
+// For each folder in ./commands
+for (const folder of commandFolders) {
+    // Get commands '.js' from subfolders
+    const commandsPath = path.join(__dirname, 'commands')
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+    
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath)
+        
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command)
+        } else {
+            console.log(`Warning! The command at ${filePath} is missing a required "data" or "execute" property!`)
+        }
+    }   
 }
 
 client.on(Events.InteractionCreate, interaction => {

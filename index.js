da7264802e0daaf45ea80b807aca0561fa083cc1
Enzo Cayetano <cayetano.enzo@icloud.com>
2023-10-24 11:24:20 -0700

    Added ready and interactionCreate

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const path = require('node:path')
 const dotenv = require('dotenv')
 dotenv.config()
 
-// Check if bot ready
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`)
-})
-
 // Create new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
@@ -39,40 +34,5 @@ for (const folder of commandFolders) {
         }
     }   
 }
-
-client.on(Events.InteractionCreate, interaction => {
-    // If not input command
-    if (!interaction.isChatInputCommand()) return
-    console.log(interaction)
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-    // If not chat input command
-    if (!interaction.isChatInputCommand()) return
-
-    const command = interaction.client.commands.get(interaction.commandName)
-
-    // No command found
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found!`)
-        return
-    }
-
-    try {
-        // Execute command
-        await command.execute(interaction)
-    } catch (error) {
-        console.error(error)
-
-        // Send error privately to user who entered command
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({content: 'There was an error while executing this command!', ephemeral: true })
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
-        }
-    }
-});
-
-
 
 client.login(process.env.DISCORD_TOKEN)

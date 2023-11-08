d90513237210764ea74ec756a323eeeda35162fd
Enzo Cayetano <cayetano.enzo@icloud.com>
2023-11-07 18:29:56 -0800

    Removed mongo/mongoose

--- events/mongo/connected.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk')
-
-module.exports = {
-  name: 'connected',
-  execute(client) {
-    console.log(chalk.green("[Database Status]: Connected."))
-  }
-}
--- events/mongo/connecting.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk')
-
-module.exports = {
-  name: 'connecting',
-  async execute(client) {
-    console.log(chalk.cyan("[Database Status]: Connecting..."))
-  }
-}

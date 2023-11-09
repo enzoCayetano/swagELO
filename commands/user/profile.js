b05d7422b02dc24affcfa9982ca5c96c5d3a2c4d
Enzo Cayetano <cayetano.enzo@icloud.com>
2023-11-09 10:27:44 -0800

    Improved embed visual

--- commands/user/profile.js
+++ commands/user/profile.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const icon = 'https://s3.amazonaws.com/challonge_app/organizations/images/000/055/281/hdpi/ARCL_Logo_Square.png?1544117144'
 const join = require('./join')
 const category = __dirname.split('/').pop()
 
@@ -16,13 +17,15 @@ module.exports = {
         // Created embed builder
         const embedProfile = new EmbedBuilder()
             .setTitle(`${interaction.user.username}'s profile`)
+            .setAuthor({ name: 'swagELO', iconURL: icon })
             .setColor(0x00f00)
             .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true, size: 1024 }))
             .addFields(
                 { name: 'Name', value: nickname, inline: true },
-                { name: 'Join Date', value: joinDate, inline: true },
                 { name: '\u200b', value: '\u200b', inline: true }, // Invisible field for spacing
+                { name: 'Join Date', value: joinDate, inline: true },
                 { name: 'ELO', value: '287', inline: true },
+                { name: '\u200b', value: '\u200b', inline: true }, // Invisible field for spacing
                 { name: 'Rank', value: 'A+', inline: true }
             )
             .setTimestamp()

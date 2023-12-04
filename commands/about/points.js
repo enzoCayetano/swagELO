9bc60b01c73a990e44c188edec79500438d9aa3a
Enzo Cayetano <cayetano.enzo@icloud.com>
2023-12-04 13:09:15 -0800

    Fixed points.js

--- commands/about/points.js
+++ commands/about/points.js
@@ -10,13 +10,13 @@ async execute(interaction) {
     const points = new EmbedBuilder()
       .setColor(0x8B0000)
       .setTitle('Current ELO Points Awarded')
-      .addFields({
-        name: 'Wins', value: '25',
-        name: 'Loss', value: '-12',
-        name: 'Kills', value: '10',
-        name: 'Death', value: '-8',
-        name: 'MVP', value: '9',
-      })
+      .addFields(
+        { name: 'Wins', value: '25' },
+        { name: 'Loss', value: '-12' },
+        { name: 'Kills', value: '10' },
+        { name: 'Death', value: '-8' },
+        { name: 'MVP', value: '9' },
+        )
 		await interaction.reply({ embeds: [points] })
 	}
 }
